f2d0b2bf7c30e10c63157aa01cdd2f1ef4376682
aditzzggg <muhamadaditsetiawan2012@gmail.com>
2026-04-13 11:44:44 +0700

    Create custom document for Next.js app

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+// pages/_document.tsx
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Custom head tags can be added here */}
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
