b4a9ed1cf5b2f01f375be6e8dd98fb5d64c2d802
aditzzggg <muhamadaditsetiawan2012@gmail.com>
2026-04-13 11:38:14 +0700

    Created pages/courses.tsx file with course listing for classes 7-12

---
+++ pages/courses.tsx
@@ -0,0 +1,44 @@
+export const courses = {
+  "7": [
+    "Mathematics",
+    "Science",
+    "Indonesian",
+    "English",
+    "Physics"
+  ],
+  "8": [
+    "Mathematics",
+    "Science",
+    "Indonesian",
+    "English",
+    "Physics"
+  ],
+  "9": [
+    "Mathematics",
+    "Science",
+    "Indonesian",
+    "English",
+    "Physics"
+  ],
+  "10": [
+    "Mathematics",
+    "Science",
+    "Indonesian",
+    "English",
+    "Physics"
+  ],
+  "11": [
+    "Mathematics",
+    "Science",
+    "Indonesian",
+    "English",
+    "Physics"
+  ],
+  "12": [
+    "Mathematics",
+    "Science",
+    "Indonesian",
+    "English",
+    "Physics"
+  ]
+};
